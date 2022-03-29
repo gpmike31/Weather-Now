ccfc46d22bc5166d489a3482e72690c75b41a3ad
gpmike31 <gpmike3105@gmail.com>
2022-03-29 14:24:55 -0500

    updated functions to capture and display weather data using template literals

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Weather-Now
+
+Weather App that will display the current weather along with a five day forcecast and will include Temp, Humidity, Wind Speed, and UV Index.
+With this app you will be able to search any city to pull up current information using the OpenWeather Api.
+
+Please click the link below for demonstration:
--- assets/script.js
+++ assets/script.js
@@ -1,97 +1,82 @@
 var apiKey = "b639d479640601578fbf359e38c5ee04";
-var htmlButton = $("#pastSearch");
+var htmlButton = $("#pastSearch")
+$("#time").text(moment().format("MMMM Do YYYY"))
 
-//moment for date. what are you gonna do with this?
-$("#time").text(moment().format("MMMM Do YYYY"));
+var pastSearch = JSON.parse(localStorage.getItem("city")) || []
+    for (let i = 0; i < pastSearch.length; i++) {
+        const pastSearchCity = pastSearch[i];
+        var buttonEl = $("<button>")
+        buttonEl.text(pastSearchCity)
+        htmlButton.append(buttonEl)
 
-var pastSearch = JSON.parse(localStorage.getItem("city")) || [];
-
-//make sure to add this loop to its own function. 
-
-// for (let i = 0; i < pastSearch.length; i++) {
-//   const pastSearchCity = pastSearch[i];
-//   var buttonEl = $("<button>");
-//   buttonEl.text(pastSearchCity);
-//   htmlButton.append(buttonEl);
-// }
+    }
 
 function handleFormSubmit(event) {
-  event.preventDefault();
-  var userInput = $(this);
-  var city = userInput[0][0].value;
-  pastSearch.push(city);
-  var buttonEl = $("<button>");
-  buttonEl.text(city);
-  htmlButton.append(buttonEl);
-  localStorage.setItem("city", JSON.stringify(pastSearch));
-  getLocation("", city);
-}
+    event.preventDefault();
+    var userInput = $(this);
+    var city = userInput[0][0].value
+    pastSearch.push(city)
+    var buttonEl = $("<button>")
+    buttonEl.text(city)
+    htmlButton.append(buttonEl)
+    localStorage.setItem("city", JSON.stringify(pastSearch))
+    getLocation("", city);
+};
 
 function getLocation(event, city) {
-  if (event) {
-    city = $(this).text();
-  }
-  console.log(city);
-  var url = `https://api.openweathermap.org/geo/1.0/direct?q=${city},&appid=${apiKey}`;
-  fetch(url)
-    .then(function (response) {
-      return response.json();
+    if (event) {
+        city = $(this).text()
+    }
+    console.log(city)
+    var url = `https://api.openweathermap.org/geo/1.0/direct?q=${city},&appid=${apiKey}`
+    fetch(url).then(function(response) {
+        return response.json()
+    }).then(function(data) {
+        console.log(data);
+        $("#city").text(city)
+        getWeather(data[0].lat, data[0].lon)
     })
-    .then(function (data) {
-      console.log(data);
-      $("#city").text(city);
-      getWeather(data[0].lat, data[0].lon);
-    });
 }
 
-function getWeather(lat, lon) {
-  var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-  fetch(url)
-    .then(function (response) {
-      return response.json();
+function getWeather(lat,lon) {
+    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+    fetch(url).then(function(response) {
+        return response.json()
+    }).then(function(data) {
+        console.log(data);
+        var fiveDayRow = $(".row")
+        var tempEl = $("#temp")
+        var humidEl = $("#humid")
+        var windEl = $("#wind")
+        var uvindexEl = $("#uvindex")
+        tempEl.text(data.current.temp)
+        humidEl.text(data.current.humidity)
+        windEl.text(data.current.wind_speed)
+        uvindexEl.text(data.current.uvi)
+        // add jquery.style to change background for uvindexel //
+        fiveDayRow.empty();
+        for (let i = 1; i < 6; i++) {
+            const element = data.daily[i];
+            
+            var fiveDayEl = `<div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">Temperature:${element.temp.max}</p>
+              <p class="card-text">Humidity:${element.humidity}</p>
+              <p class="card-text">Wind Speed:${element.wind_speed}</p>
+              <p class="card-text">UV Index:${element.uvi}</p>
+            </div>
+          </div>`
+          fiveDayRow.append(fiveDayEl)
+        }
+
     })
-    .then(function (data) {
-      console.log(data);
-      fiveDay(data);
-      currentWeather(data);
-    });
-}
-function currentWeather(data) {
-
-    //current weather should display the city name, the date, the icon, the temp, the humidiity, the wind and uvi should be a button that changes color depending on the index of the uvi
-    var temperature = data.current.temp;
-      var humidity =data.current.humidity;
-      var windspeed = data.current.wind_speed;
-      var uvindex=data.current.uvi;
-    var currentWeather = $("#currentWeather");
-
-
 }
 
-function fiveDay(data) {
-  var fiveDayRow = $(".fiveDay");
+$("#form").on("submit", handleFormSubmit)
+htmlButton.on("click", "button", getLocation)
 
-  // add jquery.style to change background for uvindexel //
-  fiveDayRow.empty();
-  for (let i = 1; i < 6; i++) {
-    const element = data.daily[i];
-// make sure you remove uvi from the five day and make sure to add the date
-    var fiveDayEl = `<div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="https://openweathermap.org/img/wn/${element.weather[0].icon}.png" alt="Card image cap">
-        <div class="card-body">
-          <p class="card-text">Temperature:${element.temp.max}</p>
-          <p class="card-text">Humidity:${element.humidity}</p>
-          <p class="card-text">Wind Speed:${element.wind_speed}</p>
-          <p class="card-text">UV Index:${element.uvi}</p>
-        </div>
-      </div>`;
-    fiveDayRow.append(fiveDayEl);
-  }
-}
 
-// need to create buttons for the search history. 
 
-$("#form").on("submit", handleFormSubmit);
-htmlButton.on("click", "button", getLocation);
 
 console.log("loaded");
